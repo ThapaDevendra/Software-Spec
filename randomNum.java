09106b2bd5e055a1f57ede1d2cca1b020e0917dc
thapadevendra <43578948+thapadevendra@users.noreply.github.com>
2019-09-09 18:32:06 -0500

    Update randomNum.java

--- randomNum.java
+++ randomNum.java
@@ -1,17 +1,16 @@
-package com.demo;
 package myapp;
 
 import java.io.IOException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+
 
 public class randomNum extends HttpServlet{
-@SuppressWarnings("serial")
+@Override
 public void doGet (HttpServletRequest req, HttpServletResponse resp) throws IOException{
     resp.setContentType("text/plain");
-    resp.getWriter.println("World");
+    resp.getWriter.println(" Devendra World");
 }
 
 }
